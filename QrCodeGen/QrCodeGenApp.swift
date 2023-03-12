9e2d662f199eabee2230a2ec20ccdd1a6e65d6ed
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-03-12 15:26:02 +0000

    Initial Commit

---
+++ QrCodeGen/QrCodeGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodeGenApp.swift
+//  QrCodeGen
+//
+//  Created by Abdoulaye Aliou SALL on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct QrCodeGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
